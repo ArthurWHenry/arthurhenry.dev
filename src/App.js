197c32614ed5522f854d659fbc7b6f232468d6fb
ArthurWHenry <awilmarhenry@gmail.com>
2021-06-01 23:48:53 -0400

    should be good for main now

--- src/App.js
+++ src/App.js
@@ -5,27 +5,33 @@ const projects = [
   {
     name: 'slicesum',
     description: 'A budgeting app designed for students and new adults.',
+    link: 'https://slicesum.com/',
   },
   {
     name: 'Streats',
     description: 'Find your favorite food truck.',
+    link: '',
   },
   {
     name: 'Theta Tau',
     description: 'Rho Gamma Chapter of Theta Tau website.',
+    link: 'https://thetataupg.org/"',
   },
   {
     name: 'localhand',
     description:
       'Hackathon project to assist small businesses in Orlando area.',
+    link: 'https://github.com/ArthurWHenry/localhand',
   },
   {
     name: 'FoodBook',
     description: 'A mobile application to manage recipes.',
+    link: 'https://github.com/ArthurWHenry/foodbook-mobile',
   },
   {
     name: 'Lego305',
     description: 'Scaled model of the Miami-Dade College Wolfson campus.',
+    link: 'https://www.youtube.com/watch?v=b8VYoZhTU6c',
   },
 ];
 
@@ -36,7 +42,7 @@ function App() {
         {/* This will be the main content. */}
         <div className="flex flex-col justify-center items-center min-h-screen space-y-6">
           {/* Introduction and small paragraph. */}
-          <main className="flex flex-col justify-start space-y-4 w-full px-4">
+          <main className="flex flex-col justify-start space-y-4 w-full px-4 pt-4">
             <h1 className="font-bold text-gray-900 text-5xl">
               Hey, I'm Arthur
             </h1>
@@ -52,36 +58,32 @@ function App() {
             {/* Going to be a grid with my projects. */}
             <div className="space-y-2">
               {/* Should be able to hover over each project and have a slight transition. */}
-              {projects.map(({ name, description }) => (
+              {projects.map(({ name, description, link }) => (
                 <div className="bg-gray-200 transition ease-linear duration-75 hover:bg-gray-300 p-4">
-                  <span className="font-semibold text-gray-700">{name}</span>
+                  <a href={link} className="font-semibold text-gray-700">
+                    {name}
+                  </a>
                   <p className="text-sm text-gray-500">{description}</p>
                 </div>
               ))}
             </div>
           </div>
           {/* Socials. */}
-          <div className="flex justify-start space-x-4 w-full px-4">
+          <div className="flex justify-start space-x-4 w-full px-4 pb-4">
             <a
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
+              href="https://github.com/ArthurWHenry"
               className="text-gray-700 transition ease-linear duration-75 hover:text-gray-800"
             >
               <FaGithub className="h-5 w-5" />
             </a>
             <a
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
+              href="https://www.linkedin.com/in/arthur-henry-0458a1104/"
               className="text-gray-700 transition ease-linear duration-75 hover:text-gray-800"
             >
               <FaLinkedin className="h-5 w-5" />
             </a>
             <a
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
+              href="https://twitter.com/arthurwhenry"
               className="text-gray-700 transition ease-linear duration-75 hover:text-gray-800"
             >
               <FaTwitter className="h-5 w-5" />
