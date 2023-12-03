8b50990149564cfb7e76b952712509d58f58da59
ArthurWHenry <awilmarhenry@gmail.com>
2023-12-03 11:53:30 -0500

    feat: added linkedin and home link with page title

--- src/components/footer/component.tsx
+++ src/components/footer/component.tsx
@@ -1,11 +1,25 @@
+import Link from "next/link";
 import { memo } from "react";
+import { FaLinkedin } from "react-icons/fa";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="flex justify-center items-center">
-      <p className="text-sm dark:text-gray-200 text-gray-900">
-        Made by Arthur Henry
-      </p>
+    <footer className="flex flex-col justify-center items-center py-4 space-y-2">
+      <div>
+        <Link
+          className="text-gray-900 dark:text-gray-50"
+          href="https://www.linkedin.com/in/arthurwhenry/"
+          target="_blank"
+          rel="noopenner noreferrer"
+        >
+          <FaLinkedin className="h-5 w-5" />
+        </Link>
+      </div>
+      <div>
+        <p className="text-sm dark:text-gray-200 text-gray-900">
+          Made by Arthur Henry
+        </p>
+      </div>
     </footer>
   );
 };
