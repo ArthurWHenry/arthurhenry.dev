6a9116aa1ede4019e5504588b9022b7af73e7d7d
ArthurWHenry <awilmarhenry@gmail.com>
2024-04-25 18:48:50 -0400

    feat: added SEO

---
+++ next-seo.config.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  openGraph: {
+    type: "website",
+    locale: "en_IE",
+    url: "https://arthurhenry.dev/",
+    site_name: "Arthur Henry",
+  },
+};
